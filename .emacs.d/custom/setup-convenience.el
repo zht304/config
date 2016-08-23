31dfb7bc11faeb8b66cb7ac8d968be8fbcc770a9
thomas <zht_304@163.com>
2016-08-23 12:47:17 +0800

    lua-mode

--- .emacs.d/custom/setup-convenience.el
+++ .emacs.d/custom/setup-convenience.el
@@ -35,7 +35,7 @@
 ;; GROUP: Convenience -> Whitespace
 
 ;; whenever you create useless whitespace, the whitespace is highlighted
-(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
+;;(add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 
 ;; activate whitespace-mode to view all whitespace characters
 (global-set-key (kbd "C-c w") 'whitespace-mode)
