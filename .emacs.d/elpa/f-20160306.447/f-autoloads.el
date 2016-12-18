084a5b9c1b793ed76dbd3f601816c7649de4fcb4
Thomas.ZHANG <zht_304@163.com>
2016-12-18 23:08:28 +0800

    remove the elpa pack folder.

--- .emacs.d/elpa/f-20160306.447/f-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("f-pkg.el" "f.el") (22283 20161 50571
-;;;;;;  251000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; f-autoloads.el ends here
