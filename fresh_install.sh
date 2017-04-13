c6485ede8b571a05c9263529756ad2394096f46e
thomaszhang <taocpp@gmail.com>
2017-04-13 22:54:40 +0800

    ...

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,12 +1,14 @@
 if [ ! -f "/etc/apt/sources.list.bak" ]; then
-	cp /etc/apt/sources.list /etc/apt/sources.list.bak
-	sed -i s/cn.archive.ubuntu.com/mirrors.163.com/ /etc/apt/sources.list
-	sed -i s/security.ubuntu.com/mirrors.163.com/ /etc/apt/sources.list
+	sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+	sudo sed -i s/cn.archive.ubuntu.com/mirrors.163.com/ /etc/apt/sources.list
+	sudo sed -i s/security.ubuntu.com/mirrors.163.com/ /etc/apt/sources.list
 fi
-apt-get update
-apt-get install vim
-apt-get install git
-apt-get install emacs
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install git
+sudo apt-get install emacs
+git config --global user.email "taocpp@gmail.com"
+git config --global user.name "thomaszhang"
 git clone http://github.com/zht304/config.git ~/config
 cd ~/config
 cp .spacemacs ~/
