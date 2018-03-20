49bd9bb735c8d80e9135deca5de3b3b27def6df3
thomaszhang <taocpp@gmail.com>
2018-03-20 19:34:30 +0800

    add mdm compiling packages

---
+++ mdm-compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install build-essential chrpath coreutils cvs desktop-file-utils diffstat docbook-utils fakeroot g++ gawk gcc git git-core help2man libgmp3-dev libmpfr-dev libreadline6-dev libtool libxml2-dev make python-pip python-pysqlite2 quilt sed subversion texi2html texinfo unzip wget
