8e04b3b98ef4da3d533e0947d8c5f2763401d131
thomas <zht_304@163.com>
2019-01-20 16:52:51 +0800

    add tools install scripts for 16.04 ubuntu.

---
+++ 16.04LTS/install_tool.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+if [ ! -f "/etc/apt/sources.list.bak" ]; then
+	sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+	sudo sed -i s/cn.archive.ubuntu.com/mirrors.163.com/ /etc/apt/sources.list
+	sudo sed -i s/security.ubuntu.com/mirrors.163.com/ /etc/apt/sources.list
+fi
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install git
+git config --global user.email "taocpp@gmail.com"
+git config --global user.name "thomaszhang"
+
+#sudo apt-get install emacs-25
+sudo apt install wget
+sudo apt install fcitx fcitx-googlepinyin fcitx-ui-classic fcitx-frontend-gtk3 fcitx-frontend-gtk
+sudo apt-get remove fcitx-frontend-qt4 fcitx-frontend-qt5 fcitx-qimpanel
+
+sudo apt-get install gedit
+sudo apt-get install android-tools-adb android-tools-fastboot
+
+sudo apt-get install libncurses5-dev
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev  libgtk-3-dev
+cd ~
+mkdir emacs-source
+cd emacs-source
+wget http://mirrors.ustc.edu.cn/gnu/emacs/emacs-25.3.tar.gz
+tar -xzf emacs-25.3.tar.gz
+./configure
+make
+sudo make install
+
+sudo apt-get install samba
+
+cd ~
+git clone http://github.com/zht304/config.git
+ln -s config/.emacs.d .emacs.d
+ln -s config/.vimrc .vimrc
+
+
+git clone http://github.com/zht304/tools.git
+
+sudo cat >> /etc/locale.gen <<EOF
+zh_CN
+zh_CN.gb18030
+zh_CN.gb2312
+zh_CN.gbk
+zh_CN.utf8
+en_US
+en_US.iso88591
+en_US.utf8
+EOF
+sudo locale-gen
+
+cat >> ~/.bashrc <<EOF
+export PATH=$PATH:/home/thomas/p4/bin
+source /home/thomas/tools/p4env
+alias ec='emacsclient -nw'
+export LC_CTYPE=zh_CN.UTF-8
+EOF
+
+cat >> ~/.xsessionrc <<EOF
+export PATH=$PATH:/home/thomas/p4/bin
+source /home/thomas/tools/p4env n
+alias ec='emacsclient -nw'
+export LC_CTYPE=zh_CN.UTF-8
+EOF
---
+++ 16.04LTS/mdm-buildtools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+
+sudo apt-get install build-essential chrpath coreutils cvs desktop-file-utils diffstat docbook-utils fakeroot g++-4.8 gawk gcc-4.8 git git-core help2man libgmp3-dev libmpfr-dev libreadline6-dev libtool libxml2-dev make python-pip python-pysqlite2 quilt sed subversion texi2html texinfo unzip wget
+
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 9
+
+
+
