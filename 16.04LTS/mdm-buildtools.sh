56ca0696e62ff59861fa2234dad0993aefa52469
thomas <zht_304@163.com>
2019-08-14 15:12:54 +0800

    update for sa415m

--- 16.04LTS/mdm-buildtools.sh
+++ 16.04LTS/mdm-buildtools.sh
@@ -2,6 +2,7 @@
 sudo apt update
 
 sudo apt-get install build-essential chrpath coreutils cvs desktop-file-utils diffstat docbook-utils fakeroot g++-4.8 gawk gcc-4.8 git git-core help2man libgmp3-dev libmpfr-dev libreadline6-dev libtool libxml2-dev make python-pip python-pysqlite2 quilt sed subversion texi2html texinfo unzip wget
+sudo apt install  libgtk2.0-bin
 
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
